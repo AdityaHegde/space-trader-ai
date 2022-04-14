3ca22bd4417fed6869483f683ccfd9704133d53f
Aditya Hegde <adityahegderocks@gmail.com>
2022-04-14 16:45:49 +0530

    Initial Commit

---
+++ src/test/java/com/adityahegde/space/trader/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityahegde.space.trader.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
